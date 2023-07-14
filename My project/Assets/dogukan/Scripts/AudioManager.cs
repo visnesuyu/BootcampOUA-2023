37a9ff5610e94acd5ef16aee2b813a53174c8599
ariffbasaran <arfbsrnn@gmail.com>
2023-07-14 23:12:50 +0300

    Last Update

---
+++ My project/Assets/dogukan/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour {
+
+	public static AudioManager instance;
+
+	public Sound[] sounds;
+
+	void Awake ()
+	{
+		if (instance != null)
+		{
+			Destroy(gameObject);
+			return;
+		} else
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.volume = s.volume;
+			s.source.pitch = s.pitch;
+			s.source.loop = s.loop;
+		}
+	}
+
+	public void Play(string sound)
+	{
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		s.source.Play();
+	}
+	public void Stop(string sound)
+	{
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		s.source.Stop();
+	}
+
+}
