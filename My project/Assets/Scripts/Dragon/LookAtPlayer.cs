bd345e4c0268dee0a463a317d8a2a1e6c8e92d94
ariffbasaran <arfbsrnn@gmail.com>
2023-07-15 21:48:21 +0300

    Archer Bug Fixed

--- My project/Assets/Scripts/Dragon/LookAtPlayer.cs
+++ My project/Assets/Scripts/Dragon/LookAtPlayer.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class LookAtPlayer : MonoBehaviour
-//{
-//    public Transform cam;
-    
-//    // Update is called once per frame
-//     void LateUpdate()
-//    {
-//        transform.LookAt(transform.position + cam.forward);
-//    }
-//}
+public class LookAtPlayer : MonoBehaviour
+{
+    public Transform cam;
+
+    //Update is called once per frame
+    void LateUpdate()
+    {
+        transform.LookAt(cam);
+    }
+}
