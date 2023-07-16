859a0dfccb4c78f1e3cf6d1fdb4cc07ad7ebf87c
visnesuyu <110824350+visnesuyu@users.noreply.github.com>
2023-07-16 19:06:17 +0300

    .buton karakter seçme ekledim deneyemedim

--- My project/Assets/Scripts/Multiplayer/SpawnPlayers.cs
+++ My project/Assets/Scripts/Multiplayer/SpawnPlayers.cs
@@ -2,18 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.UI;
 public class SpawnPlayers : MonoBehaviour
 {
     public GameObject playerPrefab;
+    public GameObject playerPrefabArcher;
+    public Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(60.53f, -0.3f, 0), Quaternion.identity);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+    public void CreateMage()
+    {
+        PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(60.53f, -0.3f, 0), Quaternion.identity);
+        Destroy(camera);
+        
+    }
+    public void CreateArcher()
+    {
+        PhotonNetwork.Instantiate(playerPrefabArcher.name, new Vector3(60.53f, -0.3f, 0), Quaternion.identity);
+        Destroy(camera);
     }
 }
