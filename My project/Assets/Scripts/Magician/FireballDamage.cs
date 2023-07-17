ea63d710a86ac1e9aba09f2f85a48435c60c65c9
ariffbasaran <arfbsrnn@gmail.com>
2023-07-17 11:59:04 +0300

    Arrow Fixed

---
+++ My project/Assets/Scripts/Magician/FireballDamage.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballDamage : MonoBehaviour
+{
+    public int damageAmount = 20;
+    public static bool isDestroy = false;
+
+
+    private void Start()
+    {   
+        if (isDestroy)
+        {
+            Destroy(gameObject, 10);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(transform.GetComponent<Rigidbody>());
+        if (other.tag == "Dragon")
+        {
+            other.GetComponent<Dragon>().TakeDamage(damageAmount);
+        }
+    }
+}
